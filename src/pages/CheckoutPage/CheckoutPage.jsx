5ee1000b8fcbdda647c0be904edb9ea75aea542b
Sofia <itssonyamisyura@gmail.com>
2026-03-14 12:23:33 +0100

    create form using React Hook Form + MUI, add validation

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,10 +1,21 @@
 import { CoffeeHero } from "../../components/CoffeeHero";
 import { Footer } from "../../components/Footer";
-
+import { useForm } from "react-hook-form";
+import TextField from "@mui/material/TextField";
 import maskGroupBg from "../../assets/maskGroupBg.png";
 
 export const CheckoutPage = ({cartCount, total}) => {
 
+    const {
+        register, 
+        handleSubmit, 
+        formState: {errors}
+    } = useForm();
+    //register подключает input
+
+    const onSubmit = (data) => {
+        console.log(data);
+      };
     return (
         <div>
             <CoffeeHero
@@ -12,12 +23,34 @@ export const CheckoutPage = ({cartCount, total}) => {
             title="Checkout"
             cartCount={cartCount}/>
              
-            <h1>Checkout</h1>
-            <p>Items: {cartCount}</p>
-            <p>Total: ${total.toFixed(2)}</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h1>Checkout</h1>
+                <p>Items: {cartCount}</p>
+                <p>Total: ${total.toFixed(2)}</p>
 
-
-
+                <TextField
+                    label='Name'
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={!!errors.name}
+                    helperText={errors.name?.message}
+                    {...register('name', {required: 'Name is required'})}
+                />
+                <TextField
+                    label='Email'
+                    variant="outlined"
+                    fullWidth
+                    margin="normal"
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
+                    {...register('email', {required: 'Email is required'})}
+                />
+                    
+                <button type="submit">
+                    Place order
+                </button>
+            </form>
             <Footer />
         </div>
     )
