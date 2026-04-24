3e4de4d772767b4771b76ff2fde0400b67bd3931
Sofia <itssonyamisyura@gmail.com>
2026-04-24 11:34:11 +0200

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/CoffeeShop">
+  <BrowserRouter basename="/CoffeeShop/">
     <HelmetProvider>
       <App />
     </HelmetProvider>
